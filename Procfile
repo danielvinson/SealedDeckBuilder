7fe02ccfa7e22855924e1d872ee35490ddacc055
Daniel Vinson <danielvinson@gmail.com>
2017-02-03 13:00:29 -0800

    app skeleton and heroku files added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python SealedDeckBuilder/app.py
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/pool/<string:pool_id>', methods=['GET'])
+def get_pool(pool_id):
+    return "Pool ID: %s" % (pool_id)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
