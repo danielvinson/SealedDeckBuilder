92f0f51e3d06134e63d1470102e88782f8318f57
Daniel Vinson <danielvinson@gmail.com>
2017-02-03 13:28:01 -0800

    Corrected port to heroku port

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def get_pool(pool_id):
     return "Pool ID: %s" % (pool_id)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
